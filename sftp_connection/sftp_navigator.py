3b4faa10b5d2538345a825b53435ad25dee45b8d
Joseph Sumi <josephsumi@gmail.com>
2024-08-28 13:00:59 -0600

    creates a directory folder if none exists

--- sftp_connection/sftp_navigator.py
+++ sftp_connection/sftp_navigator.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt
+
 from sftp_connection import SFTPConnector
 
 
@@ -9,6 +11,7 @@ def log(text):
 class SFTPNavigator:
 
     GALLERY_PATH = "./public_html/wp-content/gallery"
+    DEFAULT_CUTOFF_TIME_IN_SECONDS = 60 * 60 * 24 * 365
 
     def __init__(self, host=None, username=None, password=None, port=None, sftp_connector=SFTPConnector()):
         self._host = host
@@ -30,14 +33,23 @@ class SFTPNavigator:
             self._sftp_session = self._connector.sftp_session
         return self._sftp_session
 
-    def _create_file(self, file_name: str, default_path=True) -> None:
-        file_path = f"{self.GALLERY_PATH}/{file_name}" if default_path is True else file_name
-        self.sftp_session.mkdir(file_path)
-        log(f"File '{file_name}' created.")
+    def _create_file(self, path: str) -> None:
+        self.sftp_session.mkdir(path)
+        log(f"File '{path}' created.")
 
     def is_file_in_directory(self, directory_path: str, file_name: str) -> bool:
-        directory_contents = self.sftp_session.listdir(directory_path)
-        return file_name in directory_contents
+        """ Checks to see if file exists in directory
+
+        Returns:
+            True if file exists
+            False if file does not exist in an existing path, creating path if necessary
+        """
+        try:
+            directory_contents = self.sftp_session.listdir(directory_path)
+            return file_name in directory_contents
+        except FileNotFoundError:  # no file/directory found
+            self._create_file(directory_path)
+            return False
 
     def save_file_to_directory(self, file_data: bytes, file_path: str):
         with self.sftp_session.open(file_path, "wb+") as file:
