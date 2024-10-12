b385131a19d7b01ec42a10aba356a936e177cf0c
Joseph Sumi <josephsumi@gmail.com>
2024-10-12 13:54:59 -0600

    added condition if the image is small

--- slack_api/slack_event_api_handler.py
+++ slack_api/slack_event_api_handler.py
@@ -90,7 +90,10 @@ class SlackEventApiHandler:
         file_name = file_data["file"]["name"]
         channel_name = file_data["file"]["shares"]["public"][file_channel_id][0]["channel_name"]
 
-        file_url = file_data["file"]["thumb_1024"]
+        file_url = file_data["file"].get("thumb_1024")
+        if file_url is None:  # The image's sides are less than 1024px
+            file_url = file_data["file"]["private_url"]
+
         image = self.api_requester.get_image_data(file_url)
 
         self._save_image_to_sftp_file(image, file_name, channel_name)
