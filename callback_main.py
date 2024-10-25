67c1add15415422e4362eb193d06fedb0c2b0fd5
Joseph Sumi <josephsumi@gmail.com>
2024-10-25 08:24:56 -0600

    simplified and modified callback function

--- callback_main.py
+++ callback_main.py
@@ -1,10 +1,14 @@
-from flask import request
-
 from callback_functions import slack_events_url_endpoint_google_cloud_function, slack_events_url_endpoint_aws_lambda
 
 
+def slack_events_callback(*args):
+    return slack_events_callback_factory(args)
+
+
 def slack_events_callback_factory(*args):
-    if isinstance(args[0], request):
+    if len(args) == 1:
         return slack_events_url_endpoint_google_cloud_function(*args)
-    else:
+    elif len(args) == 2:
         return slack_events_url_endpoint_aws_lambda(*args)
+    else:  # more than two arguments returned
+        raise NotImplementedError("Expected only one or two arguments passed.")
